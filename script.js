6e406c6bc73d1acb939c3e24cddd0e1c9e69e79f
lukskyfly <lukasz.grazda@gmail.com>
2023-07-07 00:04:49 +0200

    add tools Area

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ const addNewTask = () => {
 		newTodo = document.createElement('li')
 		newTodo.textContent = todoInput.value
 		ulList.append(newTodo)
-        todoInput.value= ''
-        errorInfo.textContent = ''
+		createTooolsArea()
+		todoInput.value = ''
+		errorInfo.textContent = ''
 	} else {
 		errorInfo.textContent = ' Wpisz treść zadania'
 	}
@@ -28,5 +29,19 @@ const addNewTask = () => {
 const prepareDOMEvents = () => {
 	addBtn.addEventListener('click', addNewTask)
 }
-
+const createTooolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+}
 document.addEventListener('DOMContentLoaded', main)
